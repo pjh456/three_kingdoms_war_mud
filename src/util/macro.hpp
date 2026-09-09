097be39e018c07b76f747f87931a25f50b2a4221
pjh456 <147148383@qq.com>
2026-09-10 02:45:07 +0800

    chore: 删死宏 DEFINE_DEFAULT_VALUE_ATTRIBUTE（全仓零调用）

--- src/util/macro.hpp
+++ src/util/macro.hpp
@@ -17,12 +17,4 @@ public:                                          \
     type &get_##name() noexcept { return name; } \
     const type &get_##name() const noexcept { return name; }
 
-#define DEFINE_DEFAULT_VALUE_ATTRIBUTE(type, name, val) \
-private:                                                \
-    type name = val;                                    \
-                                                        \
-public:                                                 \
-    type &get_##name() noexcept { return name; }        \
-    const type &get_##name() const noexcept { return name; }
-
 #endif  // INCLUDE_TKW_UTIL_MACRO_HPP
